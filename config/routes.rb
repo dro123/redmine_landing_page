e1feb8d55dba6e452e1890a96c8333bcfee61239
Tobias Droste <tdroste@gmx.de>
2015-04-22 22:57:01 +0200

    Add compatibility with rails >= 4
    Rails 4 disallows 'match' in routes.rb without specifying the http method.
    Replace all 'match' with 'get'.
    
    Signed-off-by: Tobias Droste <tdroste@gmx.de>

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 if Rails::VERSION::MAJOR >= 3
-  match '/projects/:id/:overview', :to => 'projects#show', :constraints => {:overview => 'overview'}
-  match '/:main', :to => 'welcome#index', :constraints => {:main => 'welcome'}
+  get '/projects/:id/:overview', :to => 'projects#show', :constraints => {:overview => 'overview'}
+  get '/:main', :to => 'welcome#index', :constraints => {:main => 'welcome'}
 else
   ActionController::Routing::Routes.draw do |map|
     map.connect '/projects/:id/:overview', :controller => 'projects', :action => 'show', :requirements => {:overview => /overview/}
